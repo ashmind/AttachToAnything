8212f9543e274af5aabb955475c15f95d9578cc0
Andrey Shchekin <ashmind@gmail.com>
2014-12-03 21:07:38 +1300

    Clean-up: Increased version, moved extension to the Debug toolbar.

--- AttachToAnything/Properties/AssemblyInfo.cs
+++ AttachToAnything/Properties/AssemblyInfo.cs
@@ -29,8 +29,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly: InternalsVisibleTo("AttachToAnything_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001d2ee9a5dff23a0b72bbb2092248b43e7ea5846a79ef47b7464a2172ae9216c08f07c48d720b7b4783eb2aaef52bf2358d1b786d57e19f747caf00ee57a4e08c1ff256e6cc6dee56dec7c23f7eb71650e81dac85c7ab2313cc5d9eb07aa8fdcbbc58b87dd23975f2f62a1bfae8b10cd449527637bb006ac86a2a3e2cf5a282a6")]
-[assembly: InternalsVisibleTo("AttachToAnything_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001001d2ee9a5dff23a0b72bbb2092248b43e7ea5846a79ef47b7464a2172ae9216c08f07c48d720b7b4783eb2aaef52bf2358d1b786d57e19f747caf00ee57a4e08c1ff256e6cc6dee56dec7c23f7eb71650e81dac85c7ab2313cc5d9eb07aa8fdcbbc58b87dd23975f2f62a1bfae8b10cd449527637bb006ac86a2a3e2cf5a282a6")]
+// Please keep in sync with vsixmanifest
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
